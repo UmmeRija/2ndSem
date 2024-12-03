e77df286f53ea35b8eff99e288023bacdcbc1341
UmmeRija <ummerijababar@gmail.com>
2024-12-03 16:14:27 +0500

    nested list

---
+++ boxmodel/assets/css/condition.js
@@ -0,0 +1,8 @@
+let age = prompt("Enter your age")
+if(age){
+    alert("age "+>=25 && <= 40+" you are elegible ")
+}else
+{
+    alert("you are not elegible")
+    location.assign("condition.html")
+}
--- boxmodel/assets/js/eslseif.js
+++ boxmodel/assets/js/eslseif.js
@@ -1,14 +1,23 @@
-let a = parseInt( prompt("enter operent1"  ))
-let b = parseInt( prompt("enter operent 2"  ))
+let a = parseInt( prompt("enter operent1" , "0-9" ))
+let b = parseInt( prompt("enter operent 2"  , "0-9"))
 let c = prompt("enter operator" , "+-*/")
  
 if (c == "+"){
     alert("the addition of " + a + " and " + b +  "is " + (a + b))
-}else if (c == "-"){
-    alert("the substraction of " + a + " and " + b +  "is " + (a - b) )
-}else if (c == "*"){
+}
+else if (c == "-"){
+    if (b > a){
+        alert("b > a ; try again");}
+   else {
+    alert("the substraction of " + a + " and " + b +  "is " + (a - b) )}
+
+}
+else if (c == "*"){
     alert ("the multiplication of " + a + " and " + b +  "is " + (a * b) )
 }else if (c == "/"){
+    if (b == 0){
+        alert("b cant be 0 ; try again ")
+    }
     alert ("the division of " + a + " and " + b +  "is " + (a / b) )
 }else if (c !== "+","-","*","/"){
     alert("invalid operator")
