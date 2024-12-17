4d7dcee17f451f6d6135f5596d9406b991ffc9c0
UmmeRija <ummerijababar@gmail.com>
2024-12-16 20:31:03 -0800

    array

--- boxmodel/assets/js/array.js
+++ boxmodel/assets/js/array.js
@@ -1,4 +1,12 @@
-let classmate =['Ali' , 'Rija' , 'Yumna' , 'Mansoor' , 'Yousha' ,'hala' , 'afifa', 'awais' , 'abdul basit' , 'haidar' , 'abdul haq' , 'asad' , 'abdul haq'];
+let classmate =['Ali' , 'Rija' , 'Yumna' , 'Mansoor' , 'Yousha' ,'Hala' , 'Afifa', 'Awais' , 'Abdul Basit' , 'Haidar' , 'Abdul Haq' , 'Asad' , 'Abdul Haq'];
+let x = ''
+let z = 1
+
 for (let i =0; i <classmate.length ; i++ ){
-    
+    x+=`
+    <li>
+    ${z+i}&nbsp;
+    ${classmate[i]}
+    </li>`;
 }
+document.getElementById("names").innerHTML=x
--- boxmodel/assets/js/for.js
+++ boxmodel/assets/js/for.js
@@ -1,61 +1,49 @@
 let a = document.getElementById("data")
-<<<<<<< HEAD
-let b = ""
-let x = prompt("Table to write")
-let y = prompt("give the initial value")
-let z = prompt("what should be the iteration value")
 
-for (let initial =x; initial <=y; initial++){
-    if (x !== 0-9){
-        alert("please enter a number")
-    }
-    if (a>=z){
-        alert("termination value mu")
-    }
-
-    b+= `<tr>
-                <td>x</td>
-                <td>*</td>
-                <td>${y}</td>
-                <td>=</td>
-                <td>${z}</td>
-            </tr>`
-}
-
-a.innerHTML = b 
-=======
 let x = parseInt(prompt("What table you want to start"))
 let y = parseInt(prompt("Enter initial value"))
 let z = parseInt(prompt("enter termination value"))
 let b = ""
-let q = "initial*x"
+// let q = "initial*x"
 
 if(x && y && z){
+}else{
+    alert("FILL ALL FIELDS")
+    location.assign('for.html')
+}
     if(isNaN(x && y && z)){
-        if(z>y){       
+        alert("value should be number");
+        location.assign('for.html')
+ }
+
+        if(z>y){ 
+        }else{
+            alert("TERMINATION VALUE SHOULD BE GREATER THEN INITIAL VALUE")
+            location.assign('for.html')
+            
+            }
+            
+
 for (let initial=y; initial<=z; initial++){
-    // if (isNaN(x) && isNaN(y) && isNaN(z))
-    b+=` <tr>
+    if ((initial*x) % 2 === 0){
+    b+=` <tr style = "background-color : blue">
                 <td>${x}</td>
                 <td>*</td>
                 <td>${initial}</td>
                 <td>=</td>
-                <td>${q}</td>
+                <td>${initial*x}</td>
             </tr>`
+}else{
+    b+=` <tr style = "background-color: pink">
+    <td>${x}</td>
+    <td>*</td>
+    <td>${initial}</td>
+    <td>=</td>
+    <td>${initial*x}</td>
+</tr>`
+}
 }
 a.innerHTML=b
-}else{
-    alert("z must be greater to  y");
-    location.assign('for.html')
-}
-}else{
-alert("value should be number");
-location.assign('for.html')
+if(z % 2 === 0){
 
 }
-}else{
-alert("FILL ALL FIELDS");
-location.assign('for.html')
-
-}
->>>>>>> 4af861910de4ba07fd1479315fc9df5f8b05567d
