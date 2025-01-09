81c6a15f545d4b61de5318553c13709081651406
UmmeRija <ummerijababar@gmail.com>
2025-01-08 19:15:28 -0800

    incompleteWork

--- boxmodel/assets/js/project.js
+++ boxmodel/assets/js/project.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
-    $.ajax({
-        url:"assets/json/project.json",
-        type:"get",
-        success:function(data1){
-            // console.log(data.hosting);
-            let i ="";
-            $.each(data1,function(indexes,objects){
-                let catObj = objects.name.split(" ").join("").toLowerCase();
-                i+=`<div class="col-lg-3 col-md-6 mt-2">
-      <div class="card">
-        <img src="${objects.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${objects.name}</h5>
-          <!-- <p class="card-text"></p> -->
-          <a href="#" class="btn btn-primary">${catObj}</a>
-        </div>
-      </div>
-    </div>`
-        })
-$("catageory").html(i);
+  $.ajax({
+    url:"assets/json/project.json",
+    type: "get",
+    success:function(card1){
+    // console.log(card1.hosting)
+    i=""
+    $.each(card1,function(index,object){
+      let catObj = object.name.split(" ").join("").toLowerCase();
+      // console.log(catObj)
+      i+=` <div class="col-lg-3 md-6 mt-2"></div>
+        <div class="card">
+          <img src="${object.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${object.name}</h5>
+           
+            <a href="product.html?category=${catObj}" class="btn btn-primary">More</a>
+          </div>
+        </div>`
+    })
+    $("#catagory").html(i);
     }
-})
+  })
 })
